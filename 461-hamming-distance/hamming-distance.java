d65361eaf5e96cb6de17612ad28c471b953edbd7
Sangeetha2005 <sangeethacsefx@gmail.com>
2026-09-15 14:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 37.5 MB (100.00%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        
+        int c=x^y;
+        int count=0;
+
+        while(c!=0){
+            count +=c&1;
+            c=c>>1;
+        }
+        return count;
+    }
+}
